816dccf9d7326d7a35cc471fc869e220a268271e
Blank <zilisheng1996@gmail.com>
2019-11-13 08:44:32 +0800

    solve cors problem

---
+++ application/api/behavior/CORS.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\api\behavior;
+
+class CORS {
+    public function appInit(&$params)
+    {
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Headers: token, Origin, X-Requested-With, Content-Type, Accept');
+        header('Access-Control-Allow-Methods: GET, POST');
+        if(request()->isOptions()) exit();
+    }
+}
